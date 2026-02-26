90c2b398f4f6fa01f304eb1ec170c437e8122d87
Marco Antonio Silva <marconsilva@outlook.com>
2026-02-26 11:12:31 +0000

    * 'Line 3: Replaced the obsolete 'using System.Web.Mvc;' with 'using Microsoft.AspNetCore.Mvc;' to resolve the missing 'ActionResult' type, which is now in the 'Microsoft.AspNetCore.Mvc' namespace in ASP.NET Core.' in file 'ContosoUniversity\Controllers\HomeController.cs'

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models.SchoolViewModels;
 
