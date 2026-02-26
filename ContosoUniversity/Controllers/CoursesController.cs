27f9ad11e5467c60ce98543a1d60b27191d8a298
Marco Antonio Silva <marconsilva@outlook.com>
2026-02-26 11:14:40 +0000

    * 'Line 7: Replaced the obsolete 'using System.Web;' with 'using Microsoft.AspNetCore.Mvc.Rendering;' to provide access to the SelectList type, which is now in the Microsoft.AspNetCore.Mvc.Rendering namespace.' in file 'ContosoUniversity\Controllers\CoursesController.cs'

--- ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/Controllers/CoursesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
-using System.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
